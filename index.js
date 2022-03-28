1b7059ef0f686bab6816c9c8acf552444d7ffff9
Jonathan Digay <jonthan.digay1@gmail.com>
2022-03-28 17:09:34 +0800

    better

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const path = require("path");
 
 // production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "/client/build")));
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
 
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
